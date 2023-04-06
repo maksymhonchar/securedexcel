a76d2e88e26260e77a0026a3808a02a1732848bc
maksymhonchar <honchardev@gmail.com>
2023-04-06 14:58:39 +0300

    Update the way application is saved: use cell with non empty fill

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
-from collections import defaultdict, OrderedDict
+from collections import OrderedDict, defaultdict
 from copy import copy
 from typing import Any, Dict, List, Tuple
 
 import openpyxl
+from openpyxl.cell import Cell
 from openpyxl.worksheet.worksheet import Worksheet
 
 
@@ -18,29 +19,51 @@ class SpreadsheetModel(object):
             worksheet_title = worksheet.title
             self.worksheets[worksheet_title] = worksheet
 
+    @staticmethod
+    def _get_cell_with_nonempty_fill(worksheet: Worksheet) -> Cell:
+        attempts_cells: List[str] = [
+            "A1", "B1", "C1",
+            "A2", "B2", "C2",
+            "A3", "B3", "C3",
+        ]
+        for cell_location in attempts_cells:
+            cell = worksheet[cell_location]
+            if cell.fill.patternType is None:
+                continue
+            else:
+                return cell
+        # return "default" cell        
+        default_ecll_location = attempts_cells[0]
+        return worksheet[default_ecll_location]
+
     def save_spreadsheet(self, file_path: str) -> None:
         workbook = openpyxl.Workbook()
         for worksheet_title, worksheet in self.worksheets.items():
             new_worksheet = workbook.create_sheet(title=worksheet_title)
             # copy cell formatting
+            cell_with_nonempty_fill = self._get_cell_with_nonempty_fill(worksheet)
             for row_idx, row in enumerate(worksheet.iter_rows(values_only=True), start=1):
                 for col_idx, cell_value in enumerate(row, start=1):
                     new_worksheet.cell(row=row_idx, column=col_idx, value=cell_value)
-                    old_cell = worksheet.cell(row=row_idx, column=col_idx)
                     new_cell = new_worksheet.cell(row=row_idx, column=col_idx)
-                    new_cell.number_format = old_cell.number_format
-                    new_cell.font = copy(old_cell.font)
-                    new_cell.border = copy(old_cell.border)
-                    new_cell.fill = copy(old_cell.fill)
-                    new_cell.alignment = copy(old_cell.alignment)
+                    new_cell.number_format = cell_with_nonempty_fill.number_format
+                    new_cell.font = copy(cell_with_nonempty_fill.font)
+                    new_cell.border = copy(cell_with_nonempty_fill.border)
+                    new_cell.fill = copy(cell_with_nonempty_fill.fill)
+                    new_cell.alignment = copy(cell_with_nonempty_fill.alignment)
             # copy worksheet attributes
             new_worksheet.sheet_format = copy(worksheet.sheet_format)
             new_worksheet.sheet_properties = copy(worksheet.sheet_properties)
             new_worksheet.merged_cells = copy(worksheet.merged_cells)
             new_worksheet.page_margins = copy(worksheet.page_margins)
             new_worksheet.freeze_panes = copy(worksheet.freeze_panes)
+            # transfer row and column dimensions
+            for col_letter, col_dim in worksheet.column_dimensions.items():
+                new_worksheet.column_dimensions[col_letter] = copy(col_dim)
+            for row_num, row_dim in worksheet.row_dimensions.items():
+                new_worksheet.row_dimensions[row_num] = copy(row_dim)
         # remove default sheet
-        if 'Sheet' in workbook.sheetnames:  # remove default sheet
+        if 'Sheet' in workbook.sheetnames:
             workbook.remove(worksheet=workbook['Sheet'])
         # save the workbook
         workbook.save(file_path)
